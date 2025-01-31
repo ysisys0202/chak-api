0bcfc3eb33ebe52c6bfe8fab2907456ca4dfb647
이윤슬 <ysisys0202@gmail.com>
2025-01-31 09:20:06 +0900

    📝 Auth api 문서 업데이트

---
+++ src/middleware/docs/auth.ts
@@ -0,0 +1,71 @@
+import { Request, Response, NextFunction } from "express";
+
+export const signup = (req: Request, res: Response, next: NextFunction) => {
+  /* 
+      #swagger.tags = ['Auth']
+      #swagger.summary = '회원가입'
+      #swagger.description = '회원가입'
+      #swagger.parameters['body'] = {
+           in: 'body',
+           description: '회원가입 요청 데이터',
+           schema: {
+                "nickname": "넷째유저",
+                "loginId": "user4",
+                "email": "hello4@mail.com",
+                "password": "password"
+           }                  
+      #swagger.responses[201] = {
+        description: '회원가입 성공',
+        schema: {
+            "token": "생성된 토큰 값",
+            "loginId": "user4"
+        },
+      } 
+      */
+
+  next();
+};
+
+export const login = (req: Request, res: Response, next: NextFunction) => {
+  /* 
+      #swagger.tags = ['Auth']
+      #swagger.summary = '로그인'
+      #swagger.description = '로그인'
+      #swagger.parameters['body'] = {
+           in: 'body',
+           description: '로그인 요청 데이터',
+           schema: {
+                "loginId": "user4",
+                "password": "password"
+           }                  
+      #swagger.responses[200] = {
+        description: '로그인 성공',
+        schema: {
+            "token": "생성된 토큰 값",
+            "loginId": "user4"
+        },
+      } 
+      */
+};
+
+export const me = (req: Request, res: Response, next: NextFunction) => {
+  /* 
+      #swagger.tags = ['Auth']
+      #swagger.summary = '로그인 확인'
+      #swagger.description = '현재 로그인한 사용자의 정보를 반환합니다.'
+      #swagger.parameters['body'] = {
+           in: 'body',
+           description: '로그인 요청 데이터',
+           schema: {
+                "loginId": "user4",
+                "password": "password"
+           }                  
+      #swagger.responses[200] = {
+        description: '로그인 확인 성공',
+        schema: {
+            "token": "생성된 토큰 값",
+            "loginId": "user4"
+        },
+      } 
+      */
+};
--- src/router/auth.ts
+++ src/router/auth.ts
@@ -3,11 +3,21 @@ import * as authController from "../controller/auth.js";
 import { validate } from "../middleware/validators/index.js";
 import { SignupSchema, LoginSchema } from "../middleware/validators/auth.js";
 import { isAuth } from "../middleware/auth/index.js";
-
+import * as authDoc from "../middleware/docs/auth.js";
 const router = express.Router();
 
-router.post("/signup", validate(SignupSchema), authController.signup);
-router.post("/login", validate(LoginSchema), authController.login);
-router.get("/me", isAuth, authController.me);
+router.post(
+  "/signup",
+  validate(SignupSchema),
+  authDoc.signup,
+  authController.signup
+);
+router.post(
+  "/login",
+  validate(LoginSchema),
+  authDoc.login,
+  authController.login
+);
+router.get("/me", isAuth, authDoc.me, authController.me);
 
 export default router;
